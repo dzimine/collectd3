9cbcd8af34b6b4dc7537bb0ad763cb27953b8885
debuger <debuger@softwarefrontier.com>
2013-06-16 14:56:05 -0700

    Display host name in navigation

--- web/main.js
+++ web/main.js
@@ -13,9 +13,19 @@ angular.module('main', []).
 
 var NavCtrl = ['$scope', '$location', function ($s, $loc) {
 // Notice an alternative way to trigger injection. But it generates jslint warning.
+   
+   // Spec: Show the hostname in nav bar. 
+   // Keep the selected host in navbar (greyed out) when route is back to dashboard.
+   $s.hostname = "";
+   $s.$on("$routeChangeStart", function(event, next, current) {
+      console.log(next, current);
+      if (next.params.host) {
+         $s.hostname = next.params.host;
+      }
+   });
 
- $s.isActiveLocation = function (route) {
-    return route === $loc.path();
- };
+   $s.isActiveLocation = function (route) {
+      return route === $loc.path();
+   };
 
 }];
